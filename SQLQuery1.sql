7adfa209a8f7d797a92dda573b73595bb56afcc8
alexefrain17 <1432461@senati.pe>
2023-09-14 11:46:02 -0500

    cree una base de datos

---
+++ SQLQuery1.sql
@@ -0,0 +1,2 @@
+﻿create database protalweb
+use portalweb
---
+++ sesionn/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace sesionn.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
